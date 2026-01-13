9f5ca5a797dfd40c6bb0f9639963eecdf2d21573
卢晓峰 <luxiaofeng.200@163.com>
2026-01-13 11:04:57 +0800

    11

--- app/service/api/user_service/user_api.go
+++ app/service/api/user_service/user_api.go
@@ -249,3 +249,22 @@ func Logoff(c *gin.Context, userID int64) error {
 	}
 	return global.DB.Model(models.McUser{}).Where("id = ?", userID).Updates(update).Error
 }
+
+// GetUserInfoByUserID 查询用户信息（返回前清空 passwd）
+func GetUserInfoByUserID(userID int64) (user *models.McUser, error error) {
+	if userID <= 0 {
+		return nil, fmt.Errorf("用户不存在")
+	}
+	err := global.DB.Model(models.McUser{}).Where("id = ?", userID).First(&user).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("用户不存在")
+		}
+		return nil, err
+	}
+	if user.Status == 0 {
+		return nil, fmt.Errorf("账户已被锁定~")
+	}
+	user.Passwd = ""
+	return user, nil
+}
